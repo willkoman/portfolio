31e5da7bf8dc08e8b978e4735a4ce53a7c0fe644
William Krasnov <willkoman@gmail.com>
2023-02-21 22:14:39 +0000

    Accessibility Changes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,10 +4,10 @@ import {RiYoutubeLine, RiTwitterLine, RiLinkedinLine, RiGithubLine} from 'react-
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/william-krasnov/" target="_blank" rel="noopener noreferrer"><RiLinkedinLine/><i className="fab fa-linkedin"></i></a>
-        <a href="https://www.twitter.com/willkoofficial/" target="_blank" rel="noopener noreferrer"><RiTwitterLine/><i className="fab fa-twitter"></i></a>
-        <a href="https://www.youtube.com/willko/" target="_blank" rel="noopener noreferrer"><RiYoutubeLine/><i className="fab fa-youtube"></i></a>
-        <a href="https://www.github.com/willkoman/" target="_blank" rel="noopener noreferrer"><RiGithubLine/><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/william-krasnov/" alt="linkedin" input type="button" target="_blank" rel="noopener noreferrer"><RiLinkedinLine/><i className="fab fa-linkedin"></i></a>
+        <a href="https://www.twitter.com/willkoofficial/" alt="twitter" input type="button" target="_blank" rel="noopener noreferrer"><RiTwitterLine/><i className="fab fa-twitter"></i></a>
+        <a href="https://www.youtube.com/willko/" alt="youtube" input type="button" target="_blank" rel="noopener noreferrer"><RiYoutubeLine/><i className="fab fa-youtube"></i></a>
+        <a href="https://www.github.com/willkoman/" alt="github" input type="button" target="_blank" rel="noopener noreferrer"><RiGithubLine/><i className="fab fa-github"></i></a>
     </div>
   )
 }
