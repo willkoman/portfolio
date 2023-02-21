3b1a1cd9c8aece6c33e46b703af402f59f1c0819
Will Krasnov <willkoman@gmail.com>
2023-02-21 13:05:08 -0500

    cleanup and responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,19 +8,17 @@ const Header = () => {
     return (
     <header>
         <section id="header">
-        
-        <div className="container header__container">
-            <h5>Heyo, I'm</h5>
-            <h1>Will Krasnov</h1>
-            <h5 className="text-light">Game Developer and Full Stack Student</h5>
-        <CTA/>
-        
-        <div className="me">
-            <img src={Me} alt="Will Krasnov" />
-        </div>
-        <HeaderSocials/>
-        <a href="#contact" className="scroll__down">Scroll Down</a>
-        </div>
+            <div className="container header__container">
+                <h5>Heyo, I'm</h5>
+                <h1>Will Krasnov</h1>
+                <h5 className="text-light">Game Developer and Full Stack Student</h5>
+                <CTA/>
+                <div className="me">
+                    <img src={Me} alt="Will Krasnov" />
+                </div>
+                <HeaderSocials/>
+                <a href="#contact" className="scroll__down">Scroll Down</a>
+            </div>
         </section>
     </header>
     )
