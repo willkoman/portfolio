4b4dc2f76b6f2eeba02151b6561cecdde6cd9df7
Will Krasnov <willkoman@gmail.com>
2023-02-20 21:00:15 -0500

    Fixed Nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,13 +7,14 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
     return (
     <header>
-        <section className="header">
+        <section id="header">
+        
         <div className="container header__container">
             <h5>Heyo, I'm</h5>
             <h1>Will Krasnov</h1>
             <h5 className="text-light">Game Developer and Full Stack Student</h5>
         <CTA/>
-       
+        
         <div className="me">
             <img src={Me} alt="Will Krasnov" />
         </div>
