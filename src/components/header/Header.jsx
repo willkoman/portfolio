5371f90066a4efbfdd00d8124810cbbeda491d8d
William Krasnov <willkoman@gmail.com>
2024-11-04 14:02:04 -0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <div className="container header__container">
           <h3>Heyo, I'm</h3>
           <h1>Will Krasnov</h1>
-          <h5 className="text-light">Game Developer and Full Stack Student</h5>
+          <h5 className="text-light">Game Developer and Full Stack Engineer</h5>
           <CTA />
           <div className="me">
             <img src={Me} alt="Will Krasnov" />
