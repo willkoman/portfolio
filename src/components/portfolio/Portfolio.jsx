4c3e597b7a09b6f54ea3e155924b40ac9fb6d1b8
William Krasnov <willkoman@gmail.com>
2024-11-05 10:04:59 -0500

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -32,6 +32,7 @@ const Portfolio = () => {
               rel="noopener noreferrer"
             >
             <img src="https://images.unsplash.com/photo-1522276498395-f4f68f7f8454?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="placeholder" />
+            </a>
           </div>
         </div>
         <div className="port_row">
